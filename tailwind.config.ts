7f6c87d6da58a4ccc71f3ab8a0159396dcc13980
k3v1bvo <79926842+k3v1bvo@users.noreply.github.com>
2025-11-07 09:46:15 -0400

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,33 +2,11 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./app/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./pages/**/*.{ts,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   darkMode: "class",
-  theme: {
-    extend: {
-      colors: {
-        border: "var(--border)",
-        input: "var(--input)",
-        ring: "var(--ring)",
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        primary: "var(--primary)",
-        secondary: "var(--secondary)",
-        destructive: "var(--destructive)",
-        accent: "var(--accent)",
-        muted: "var(--muted)",
-        card: "var(--card)"
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)"
-      }
-    }
-  },
+  theme: { extend: {} },
   plugins: []
 };
 
